1ce80886db49e4631b8b4751389219519cf19615
Krompy <kompennet@gmail.com>
2017-04-14 23:43:46 +0300

    Start with smth simple

---
+++ Lab_2-2/Drawers/LineDrawer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_2_2.Drawers
+{
+    class LineDrawer : IDrawer
+    {
+        public void Draw()
+        {
+            //TODO
+        }
+    }
+}
---
+++ Lab_2-2/MainWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Lab_2_2.Drawers;
+
+namespace Lab_2_2
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public enum Figures { line, rectangle, square, ellipse, circle };
+        private delegate IDrawer DrawerCreator();
+        struct Controller
+        {
+            public string Name { get; set; }
+            public DrawerCreator Creator { get; set; }
+            public Figures FigureType;
+
+        }
+        Point firstPoint, endPoint;
+        bool firstClick = true;
+        bool secondClick = false;
+        bool buf = false;
+        Shape currShape = new Shape();
+        Figures chosenType;
+        private Dictionary<int, Controller> DrawerDictionary;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            DrawerDictionary = new Dictionary<int, Controller>();
+            DrawerDictionary.Add(0, new Controller() { Name = "line", FigureType = Figures.line, Creator = () => { return new LineDrawer(); } });
+        }
+    }
+}
